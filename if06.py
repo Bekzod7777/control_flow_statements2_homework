8660b1685bc56addbbd569f626feb3e8b69beae9
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-25 19:02:46 +0500

    6m

--- if06.py
+++ if06.py
@@ -6,4 +6,21 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    besh=n%10
+    tort=n//10
+    tort1=tort%10
+    uch=tort//10
+    uch1=uch%10
+    ikki=uch//10
+    ikki1=ikki%10
+    bir=ikki//10
+    if besh>=tort1 and besh>=uch1 and besh>=ikki1 and besh>=bir:
+        return 1
+    elif tort1>=besh and tort1>=uch1 and tort1>=ikki1 and tort1>=bir:
+        return 2
+    elif uch1>=besh and uch1>=tort1 and uch1>=ikki1 and uch1>=bir:
+        return 3
+    elif ikki1>=besh and ikki1>=tort1 and ikki1>=uch1 and ikki1>=bir:
+        return 4
+    else:
+        return 5
